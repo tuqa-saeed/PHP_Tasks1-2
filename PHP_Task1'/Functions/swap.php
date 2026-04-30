79b3c99e6469c88584d02ef80e72edb05ba750b7
tuqa-saeed <tuqasaeed3@gmail.com>
2026-04-30 15:17:47 +0300

    Refactor swap function into SwapTwoNum class

--- PHP_Task1'/Functions/swap.php
+++ PHP_Task1'/Functions/swap.php
@@ -1,17 +1,29 @@
 <?php
-echo "<div style='background-color:lightblue; color: white; font-size: 20px; font-weight: bold; padding: 10px; text-align: center; border-radius: 5px;  margin: 10px auto;'>
-============ Swap ================</div>";
-/* Sample Input:  x = 12     y= 10 
-Expected Output: y=12   x=10  */
-function swap(&$a , &$b)
-{
-$a= $a ^ $b;
-$b=$a ^ $b;
-$a=$a ^ $b;
+
+class SwapTwoNum {
+
+    private $a, $b;
+
+    public function __construct($a, $b) {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function swapnum() {
+        $this->a = $this->a + $this->b;
+        $this->b = $this->a - $this->b;
+        $this->a = $this->a - $this->b;
+    }
+
+    public function getResult() {
+        return "The Result a = " . $this->a . " , The Result b = " . $this->b;
+    }
 }
-$a = 12;
-$b = 10;
-swap($a,$b);
-echo "x= $a  y= $b";
+
+
+// usage
+$swap = new SwapTwoNum(12, 10);
+$swap->swapnum();
+echo $swap->getResult();
 
 ?>
